5a2f5cb4620bdcb43adfed0d4f400eaf2258efe3
sarahcrack <sarah.l.crack@googlemail.com>
2023-05-31 17:31:25 +0100

    added styling to ColouredBoxes and text/styling to footer

--- src/Components/ColouredBoxes/ColouredBoxes.js
+++ src/Components/ColouredBoxes/ColouredBoxes.js
@@ -1,3 +1,31 @@
+// import React from "react";
+// import "./ColouredBoxes.css";
+// import { Flex, Box } from "@chakra-ui/react";
+
+// function ColouredBoxes({
+//   bgLeft,
+//   bgRight,
+//   headingLeft,
+//   headingRight,
+//   leftImage,
+//   rightImage,
+// }) {
+//   return (
+//     <Flex>
+//       <Box bg={bgLeft} flex="1" height="50vh">
+//         {leftImage && <img src={leftImage} alt="Left Box" />}
+//         <div className="headingLeft">{headingLeft}</div>
+//       </Box>
+//       <Box bg={bgRight} flex="1" height="50vh">
+//         {rightImage && <img src={rightImage} alt="Right Box" />}
+//         <div className="headingRight">{headingRight}</div>
+//       </Box>
+//     </Flex>
+//   );
+// }
+
+// export default ColouredBoxes;
+
 import React from "react";
 import "./ColouredBoxes.css";
 import { Flex, Box } from "@chakra-ui/react";
@@ -12,11 +40,27 @@ function ColouredBoxes({
 }) {
   return (
     <Flex>
-      <Box bg={bgLeft} flex="1" height="50vh">
+      <Box
+        bg={bgLeft}
+        flex="1"
+        height="50vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
         {leftImage && <img src={leftImage} alt="Left Box" />}
         <div className="headingLeft">{headingLeft}</div>
       </Box>
-      <Box bg={bgRight} flex="1" height="50vh">
+      <Box
+        bg={bgRight}
+        flex="1"
+        height="50vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
         {rightImage && <img src={rightImage} alt="Right Box" />}
         <div className="headingRight">{headingRight}</div>
       </Box>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,46 @@
 import React from "react";
 import { Flex, Box } from "@chakra-ui/react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <Flex>
-      <Box bg="#F5D5ED" flex="1" height="10vh"></Box>
-    </Flex>
+    <div className="Footer">
+      <Flex>
+        <Box
+          bg="#F5D5ED"
+          flex="1"
+          height="10vh"
+          display="flex"
+          alignItems="flex-end"
+        >
+          <Flex justifyContent="space-between" alignItems="center" width="100%">
+            <Box className="myName" marginLeft={30} mb={5}>
+              © 2023 Sarah Crack
+            </Box>
+            <Flex alignItems="center">
+              <Box className="myLinks" marginRight={70} mb={5}>
+                This project was coded in React by{" "}
+                <a
+                  href="https://www.linkedin.com/in/sarahcrack/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Sarah Crack{" "}
+                </a>
+                and is{" "}
+                <a
+                  href="https://github.com/sarahcrack"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  open-sourced
+                </a>
+              </Box>
+            </Flex>
+          </Flex>
+        </Box>
+      </Flex>
+    </div>
   );
 }
 
