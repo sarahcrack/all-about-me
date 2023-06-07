d85650bea031f8ea0700caca2fc21983e0bec3bb
sarahcrack <sarah.l.crack@googlemail.com>
2023-06-07 13:53:39 +0100

    added ChakraUI card template to Projects page

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,4 +1,16 @@
 import Hero from "../Components/Hero/Hero";
+import {
+  Card,
+  Stack,
+  Image,
+  CardBody,
+  CardFooter,
+  Button,
+  Heading,
+  Text,
+  ButtonGroup,
+  Divider,
+} from "@chakra-ui/react";
 
 function Projects() {
   const heroHeading = (
@@ -10,13 +22,45 @@ function Projects() {
   );
 
   return (
-    <div>
+    <div className="projects">
       <Hero
         bg="#7A9E9F"
         colour="#FCDDEC"
         heroHeading={heroHeading}
         heroDescription="A few projects I  loved working on 🤩"
       />
+
+      <Card maxW="md">
+        <CardBody>
+          <Image
+            src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            alt="Green double couch with wooden legs"
+            borderRadius="lg"
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">Living room Sofa</Heading>
+            <Text>
+              This sofa is perfect for modern tropical spaces, baroque inspired
+              spaces, earthy toned spaces and for people who love a chic design
+              with a sprinkle of vintage design.
+            </Text>
+            <Text color="blue.600" fontSize="2xl">
+              $450
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup spacing="2">
+            <Button variant="solid" colorScheme="blue">
+              Buy now
+            </Button>
+            <Button variant="ghost" colorScheme="blue">
+              Add to cart
+            </Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
